6d36531fbc589eb54c2766c255ea3583569cf2b9
Vandad Nahavandipoor <vandad.np@gmail.com>
2014-12-22 12:06:54 +0100

    structure of the new issue

---
+++ issue07/exampleCode/buildRelease.sh
@@ -0,0 +1,8 @@
+rm -rf build
+xcrun xcodebuild -sdk iphonesimulator -configuration Release -arch x86_64
+rm -rf ~/Desktop/output/
+mkdir ~/Desktop/output
+cp -r build/Release-iphonesimulator/swift-weekly.app ~/Desktop/output/
+cp -r build/Release-iphonesimulator/swift-weekly.app.dSYM ~/Desktop/output/
+open ~/Desktop/output/
+
---
+++ issue07/exampleCode/swift-weekly/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  swift-weekly
+//
+//  Created by vandadnp on 12/22/14.
+//  Copyright (c) 2014 com.pixolity.ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
