04a81f8266d96e2a23c0553399d94274583b70a7
Vandad Nahavandipoor <vandad.np@gmail.com>
2015-01-11 21:20:52 +0100

    Adding issue08

--- issue08/exampleCode/swift-weekly/ViewController.swift
+++ issue08/exampleCode/swift-weekly/ViewController.swift
@@ -8,12 +8,38 @@
 
 import UIKit
 
+struct Person{
+    var name: String
+}
+
+struct Car{
+    var manufacturingYear: UInt
+}
+
 class ViewController: UIViewController {
+    
+    func example1(){
+        let items = [
+            0xabcdefa,
+            "Foo",
+            0xabcdefb,
+            "Bar"
+        ]
+        
+        for v in items{
+            if v is Int{
+                println(0xabcdefc)
+            }
+            else if v is String{
+                println(0xabcdefd)
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-
+        
+        example1()
     }
 
 }
