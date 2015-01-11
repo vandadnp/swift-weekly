60aeca95620fa0f1554c1d05ef20725a0c45d3ab
Vandad Nahavandipoor <vandad.np@gmail.com>
2015-01-11 21:15:57 +0100

    Adding issue08

---
+++ issue08/exampleCode/swift-weekly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-weekly
+//
+//  Created by vandadnp on 11/01/15.
+//  Copyright (c) 2014 com.pixolity.ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+}
+
