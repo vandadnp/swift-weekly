5bf1347a67ef671da220fb81d32ef372d2c91220
Vandad Nahavandipoor <vandad.np@gmail.com>
2015-02-03 11:10:55 +0100

    Added some more information about the count() method

--- issue09/exampleCode/swift-weekly/ViewController.swift
+++ issue09/exampleCode/swift-weekly/ViewController.swift
@@ -10,68 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let items = [
-        0xabcdefa,
-        "Foo",
-        0xabcdefb,
-        "Bar"
-    ]
+    let array = ["Vandad",0xabcdefa, "Julian",0xabcdefb, "Leif", 0xabcdefc]
+        as [AnyObject] //done to ensure no implicit NSArray conversion is happening
+    
+    func randomIndexInArray(a: [AnyObject]) -> Int{
+        return Int(arc4random_uniform(UInt32(a.count)))
+    }
     
     func example1(){
         
-        for v in items{
-            
-            if v is Int{
-                println(0xabcdefc)
-            }
-            else if v is String{
-                println(0xabcdefd)
-            }
+        for i in 0..<array.count{
+            let index = randomIndexInArray(array)
+            let obj: AnyObject = array[index]
+            println(obj)
         }
         
-    }
-    
-    class Vehicle{
-        func id() -> Int{
-            return 0xabcdefa
-        }
-    }
-    
-    class Car : Vehicle{
-        override func id() -> Int {
-            return 0xabcdefc
-        }
-    }
-    
-    func example2(){
-        let v: Vehicle = Car()
-        let c = [v][0] as Car
-        println(c)
-    }
-    
-    class Bicycle : Vehicle{
-        override func id() -> Int {
-            return 0xabcdefb
-        }
-    }
-    
-    func example3(){
-        
-        let items = [Bicycle(), Car(), Bicycle()]
-        for i in items{
-            if let b = i as? Bicycle{
-                println(0xabcdefd)
-            }
-            else if let c = i as? Car{
-                println(0xabcdefe)
-            }
-        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        example2()
+        example1()
+        
     }
     
 }
