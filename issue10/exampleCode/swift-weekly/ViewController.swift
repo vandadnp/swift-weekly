73ae2a91d573a6684bfdf08ae3a85153d291d410
Vandad Nahavandipoor <vandad.np@gmail.com>
2015-02-18 15:04:40 +0100

    Added the skeleton for issue 10

---
+++ issue10/exampleCode/swift-weekly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-weekly
+//
+//  Created by vandadnp on 18/02/15.
+//  Copyright (c) 2015 com.pixolity.ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
